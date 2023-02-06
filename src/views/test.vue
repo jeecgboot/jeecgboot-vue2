14640e6d315e9d9a719d5d4843babd0d9b5681cd
Tony <shysoong@gmail.com>
2023-02-06 09:37:41 +0800

    - add test

---
+++ src/views/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <a-scene background="color: #ECECEC">
+      <a-assets>
+        <a-asset-item id="cityModel" src="../assets/models/01.glb"></a-asset-item>
+      </a-assets>
+      <a-entity gltf-model="#cityModel" position="-1 0.5 -3"></a-entity>
+    </a-scene>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  methods: {
+    test() {
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
